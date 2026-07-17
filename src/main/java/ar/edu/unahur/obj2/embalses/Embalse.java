d51e4401d344f4e71b9eda3e87d7e9b87acb0a61
javi201 <javicasco93@gmail.com>
2026-07-17 18:55:34 -0300

    Creación de clase embalse

--- src/main/java/ar/edu/unahur/obj2/embalses/Embalse.java
+++ src/main/java/ar/edu/unahur/obj2/embalses/Embalse.java
@@ -4,5 +4,18 @@ package ar.edu.unahur.obj2.embalses;
  * Embalse
  */
 public class Embalse {
+    private Integer identificador;
+    private Integer volAguaActual;
 
+    public Integer getVolAguaActual() {
+        return this.volAguaActual;
+    }
+
+    public void inyectarAgua(Integer volAdicional) {
+        this.volAguaActual += volAdicional;
+    }
+
+    public void extraerAgua(Integer volARestar) {
+        this.volAguaActual -= volARestar;
+    }
 }
