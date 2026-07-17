2c0fd9594eaaeefdd88cff51c52cd07a93bde897
javi201 <javicasco93@gmail.com>
2026-07-17 19:08:08 -0300

    Planteo de comando

---
+++ src/main/java/ar/edu/unahur/obj2/embalses/Comando.java
@@ -0,0 +1,6 @@
+package ar.edu.unahur.obj2.embalses;
+
+public interface Comando {
+    void ejecutar();
+    void deshacer();
+}
---
+++ src/main/java/ar/edu/unahur/obj2/embalses/Inyeccion.java
@@ -0,0 +1,14 @@
+package ar.edu.unahur.obj2.embalses;
+
+public class Inyeccion implements Comando {
+    private Embalse embalse;
+
+    @Override
+    public void ejecutar() {
+    }
+
+    @Override
+    public void deshacer() {
+    }
+
+}
